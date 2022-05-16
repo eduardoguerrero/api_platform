af3c323c52859cb83c452887abb6211c062b740b
Rene <Escobar>
2022-05-16 00:14:22 -0500

    Task add filters

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -2,11 +2,29 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={
+ *     "get"={},
+ *     "put",
+ *     "delete"
+ * },
+ * shortName="cheeses",
+ * normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
+ * denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"}
+ * )
+ * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
+ * @ApiFilter(SearchFilter::class, properties={"title": "partial", "description": "partial"})
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
  */
 class CheeseListing
@@ -20,16 +38,19 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"cheese_listing:read","cheese_listing:write"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"cheese_listing:read"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="integer")
+     * @Groups({"cheese_listing:read","cheese_listing:write"})
      */
     private $price;
 
@@ -41,42 +62,89 @@ class CheeseListing
     /**
      * @ORM\Column(type="boolean")
      */
-    private $isPublished;
+    private $isPublished = false;
 
+    /**
+     * @param string|null $title
+     */
+    public function __construct(string $title = null)
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        // Use the same property name as constructor argument, otherwise error
+        $this->title = $title;
+    }
+
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getTitle(): ?string
     {
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    /**
+     *  From an object-oriented perspective, this makes the title property immutable:
+     *
+     * @param string $title
+     * @return $this
+     */
+    /*public function setTitle(string $title): self
     {
         $this->title = $title;
 
         return $this;
-    }
+    }*/
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    /*public function setDescription(string $description): self
     {
         $this->description = $description;
 
         return $this;
+    }*/
+
+    /**
+     * The description of the cheese as raw text
+     *
+     * @Groups({"cheese_listing:write"})
+     * @SerializedName("description")
+     * @param string $description
+     * @return $this
+     */
+    public function setTextDescription(string $description): self
+    {
+        $this->description = nl2br($description);
+
+        return $this;
     }
 
+    /**
+     * @return int|null
+     */
     public function getPrice(): ?int
     {
         return $this->price;
     }
 
+    /**
+     * @param int $price
+     * @return $this
+     */
     public function setPrice(int $price): self
     {
         $this->price = $price;
@@ -84,23 +152,44 @@ class CheeseListing
         return $this;
     }
 
+    /**
+     * @return \DateTimeInterface|null
+     */
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    /**
+     * How long ago in text that this cheese listing was added
+     *
+     * @Groups({"cheese_listing:read"})
+     * @return string
+     */
+    public function getCreatedAtAgo(): string
+    {
+        return Carbon::instance($this->createdAt)->diffForHumans();
+    }
+
+    /*public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
 
         return $this;
-    }
+    }*/
 
+    /**
+     * @return bool|null
+     */
     public function getIsPublished(): ?bool
     {
         return $this->isPublished;
     }
 
+    /**
+     * @param bool $isPublished
+     * @return $this
+     */
     public function setIsPublished(bool $isPublished): self
     {
         $this->isPublished = $isPublished;
